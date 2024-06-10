8e4a26ecdd225ef5f1022491e83880b9ea72685b
Neil Varia <neilvaria@gmail.com>
2024-06-10 02:49:42 +0100

    removed prisma migrations

--- prisma/migrations/20230403082806_rooms/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "Room" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL
-);
--- prisma/migrations/20230501164641_added_completion_date/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Tasks" ADD COLUMN "completionDate" DATETIME;
